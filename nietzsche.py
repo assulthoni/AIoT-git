9769f5eedd3935c04cf62ce68658eb8a8959e3d2
Hanz Jefferson <varkhan245@gmail.com>
2024-06-01 17:38:07 +0700

    The Master of Nihilism

---
+++ nietzsche.py
@@ -0,0 +1,26 @@
+import time
+
+quotes = [
+    "He who has a why to live can bear almost any how.",
+    "That which does not kill us makes us stronger.",
+    "Without music, life would be a mistake.",
+    "In individuals, insanity is rare; but in groups, parties, nations and epochs, it is the rule.",
+    "There are no facts, only interpretations.",
+    "One must still have chaos in oneself to be able to give birth to a dancing star.",
+    "To live is to suffer, to survive is to find some meaning in the suffering.",
+    "It is not a lack of love, but a lack of friendship that makes unhappy marriages.",
+    "The individual has always had to struggle to keep from being overwhelmed by the tribe. If you try it, you will be lonely often, and sometimes frightened. But no price is too high to pay for the privilege of owning yourself.",
+    "Without cruelty, there is no festival: thus the longest and most ancient part of human history teaches."
+    "God is dead. God remains dead. And we have killed him. How shall we comfort ourselves, the murderers of all murderers?",
+    "I cannot believe in a God who wants to be praised all the time.",
+    "The surest way to corrupt a youth is to instruct him to hold in higher esteem those who think alike than those who think differently.",
+    "To predict the behavior of ordinary people in advance, you only have to assume that they will always try to escape a disagreeable situation with the smallest possible expenditure of intelligence.",
+    "The essence of all beautiful art, all great art, is gratitude.",
+    "A thinker sees his own actions as experiments and questions--as attempts to find out something. Success and failure are for him answers above all.",
+    "The true man wants two things: danger and play. For that reason he wants woman, as the most dangerous plaything.",
+    "He who fights with monsters should look to it that he himself does not become a monster. And if you gaze long into an abyss, the abyss also gazes into you."
+]
+
+for i in quotes:
+    print(quotes[i])
+    time.sleep(0.5)
